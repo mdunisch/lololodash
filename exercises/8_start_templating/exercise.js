49e9a1d32b2e429f7e81a7ea50d2f1f111f5ab6a
mdunisch <m.dunisch@confixmedia.de>
2014-08-09 11:27:32 +0200

    Adding exercise 8

---
+++ exercises/8_start_templating/exercise.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var verify = require("../../lib/verify.js");
+var _ = require("lodash");
+
+var run = {
+
+    json: {
+        name: "Tom",
+        login: [
+            1407574431, 140753421, 140715429
+        ]
+    },
+    expect: "Hello Tom (logins: 3)"
+};
+
+
+var testing = {
+    'Checking with standard dataset...': {
+        input: run.json,
+        shouldbe: run.expect
+    },
+    'Checking without logins': {
+        input: {
+            name: "Tom",
+            login: []
+        },
+        shouldbe: "Hello Tom (logins: 0)"
+    }
+};
+
+module.exports = verify(testing,run);
