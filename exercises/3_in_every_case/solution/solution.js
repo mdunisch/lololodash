dab65d28c7e1058415798f85ba761c9c3f41611d
mdunisch <m.dunisch@confixmedia.de>
2015-10-07 10:51:27 +0200

    Fix https://github.com/mdunisch/lololodash/issues/30

--- exercises/3_in_every_case/solution/solution.js
+++ exercises/3_in_every_case/solution/solution.js
@@ -4,16 +4,13 @@ var _ = require("lodash");
 
 module.exports = function(collection) {
   // add a size attribute to the collection based on the item's population
-  // >= 1.0 (million) is "big"
-  // >= 0.5 && < 1.0 is "med"
-  // < 0.5 is "small"
   return _.forEach(collection, function(item) {
-    if (item.population >= 1) {
+    if (item.population > 1) {
       item.size = "big";
-    } else if(item.population < 0.5) {
-      item.size = "small";
-    } else {
+    } else if(item.population > 0.5) {
       item.size = "med";
+    } else {
+      item.size = "small";
     }
   });
 };
